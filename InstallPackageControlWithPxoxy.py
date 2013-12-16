414e4f40545a9da8b1df41b55f675a4c64bbc558
longly <ll521nn@gmail.com>
2013-12-16 11:41:30 +0800

    add proxy etc

--- InstallPackageControlWithPxoxy.py
+++ InstallPackageControlWithPxoxy.py
@@ -1,5 +1,5 @@
 # replace the offcial website python code with thi
+#replace protocol,username,password,,host,port with youself
+#This only add proxy  depend on offcial website 
 
-
-
-import urllib2,os; pf='Package Control.sublime-package'; ipp = sublime.installed_packages_path(); os.makedirs( ipp ) if not os.path.exists(ipp) else None; urllib2.install_opener( urllib2.build_opener( urllib2.ProxyHandler({"http":"http://weixianlong:weixianlong@192.168.16.189:8080"}))); open( os.path.join( ipp, pf), 'wb' ).write( urllib2.urlopen( 'http://sublime.wbond.net/' +pf.replace( ' ','%20' )).read()); print( 'Please restart Sublime Text to finish installation')
+import urllib2,os; protocol='http';username='username';password='pwd';host='192.168.1.112';port='8080'; porxyadd = '%s://%s:%s@%s:%s' %  (protocol,username,password,host,port) ; pf='Package Control.sublime-package'; ipp = sublime.installed_packages_path(); os.makedirs( ipp ) if not os.path.exists(ipp) else None; urllib2.install_opener( urllib2.build_opener( urllib2.ProxyHandler({protocol : porxyadd}))); open( os.path.join( ipp, pf), 'wb' ).write( urllib2.urlopen( 'http://sublime.wbond.net/' +pf.replace( ' ','%20' )).read()); print( 'Please restart Sublime Text to finish installation')
